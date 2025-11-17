b84dea3f56d0a9605a7711746f2887558b35187e
manaralthqfiu <9vieo677@gmail.com>
2025-11-17 14:58:00 +0000

    add

---
+++ printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format
+ * @format: Format string
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (*format == 's')
+				count += print_string(va_arg(args, char *));
+			else if (*format == '%')
+				count += _putchar('%');
+			else
+				return (-1);
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
+
