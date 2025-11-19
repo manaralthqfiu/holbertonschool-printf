57183ae04043261d1f6c86062dc235770a8a0f6b
iihaad9 <fahadfawaz5@gmail.com>
2025-11-19 10:29:34 +0000

    Implement functions.c helpers

--- functions.c
+++ functions.c
@@ -1,27 +1,52 @@
-
-
+#include "main.h"
 /**
- * _putchar - Writes a character to stdout
- * @c: Character to print
- *
- * Return: 1 on success
- */
+* _putchar - Writes a character to stdout
+* @c: Character to print
+*
+* Return: 1 on success
+*/
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
-
 /**
- * print_string - Prints a string
- * @str: String to print
- *
- * Return: Number of characters printed
- */
+* print_string - Prints a string
+* @str: String to print
+*
+* Return: Number of characters printed
+*/
 int print_string(char *str)
 {
-    int count = 0;
-    int i = 0;
-
-    if (!str)
-        str = "(null)";
-
+int count = 0;
+int i = 0;
+if (!str)
+str = "(null)";
+while (str[i])
+{
+count += _putchar(str[i]);
+i++;
+}
+return (count);
+}
+/**
+* print_number - Prints an integer
+* @n: Integer to print
+*
+* Return: Number of characters printed
+*/
+int print_number(int n)
+{
+int count = 0;
+unsigned int num;
+if (n < 0)
+{
+count += _putchar('-');
+num = -n;
+}
+else
+num = n;
+if (num / 10)
+count += print_number(num / 10);
+count += _putchar((num % 10) + '0');
+return (count);
+}
