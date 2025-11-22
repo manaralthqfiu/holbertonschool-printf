ffb2bc9c3342b9529c2049d8ddb866234d9e2f45
iihaad9 <fahadfawaz5@gmail.com>
2025-11-22 12:39:56 +0000

    Add 1024-byte buffer to _putchar (advanced task)

--- functions.c
+++ functions.c
@@ -1,52 +1,78 @@
 #include "main.h"
+#include <unistd.h>
+
 /**
-* _putchar - Writes a character to stdout
-* @c: Character to print
-*
-* Return: 1 on success
-*/
+ * _putchar - buffered character output (1024 bytes buffer)
+ * @c: character to print (if c == -1, flush buffer)
+ *
+ * Return: 1 always
+ */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+    static char buffer[1024];
+    static int index;
+    int printed = 1;
+
+    /* إذا وصل البافر 1024 أو إذا نادينا -1 → نفضّيه */
+    if (c == -1 || index == 1024)
+    {
+        write(1, buffer, index);
+        index = 0;
+    }
+
+    /* إذا c مو -1 → نضيفه للبفر */
+    if (c != -1)
+        buffer[index++] = c;
+
+    return (printed);
 }
+
 /**
-* print_string - Prints a string
-* @str: String to print
-*
-* Return: Number of characters printed
-*/
+ * print_string - print a string
+ * @str: string to print
+ *
+ * Return: number of chars printed
+ */
 int print_string(char *str)
 {
-int count = 0;
-int i = 0;
-if (!str)
-str = "(null)";
-while (str[i])
-{
-count += _putchar(str[i]);
-i++;
+    int count = 0;
+    int i = 0;
+
+    if (!str)
+        str = "(null)";
+
+    while (str[i])
+        count += _putchar(str[i++]);
+
+    return (count);
 }
-return (count);
-}
+
 /**
-* print_number - Prints an integer
-* @n: Integer to print
-*
-* Return: Number of characters printed
-*/
+ * print_number - print an integer
+ * @n: integer to print
+ *
+ * Return: number of chars printed
+ */
 int print_number(int n)
 {
-int count = 0;
-unsigned int num;
-if (n < 0)
-{
-count += _putchar('-');
-num = -n;
+    int count = 0;
+    unsigned int num;
+
+    if (n < 0)
+    {
+        count += _putchar('-');
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
+
+    if (num / 10)
+        count += print_number(num / 10);
+
+    count += _putchar((num % 10) + '0');
+
+    return (count);
 }
-else
-num = n;
-if (num / 10)
-count += print_number(num / 10);
-count += _putchar((num % 10) + '0');
-return (count);
-}
+
--- printf.c
+++ printf.c
@@ -44,6 +44,7 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(args);
+	_putchar(-1);
 	return (count);
 }
 
